58ca980e42997d22bfb960f5af6d34daebfe4ab8
danielukd <danielukd@gmail.com>
2018-01-21 16:40:15 +0100

    dodane więcej flag

--- skrypt.sh
+++ skrypt.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
+function manual(){
+	
+}
 
-
-
-#sprawdzanie liczby argumentow
-if [ "$#" -eq 3 ]; then
-    while getopts "rc" opt; do
-		case $opt in
-		r)
-			echo "mam flage r - szukanie rekursywnie"
-			;;
-		c)
-			echo "mam flage c - kolorowanie wyniku"
-			;;
-		esac
-	done
+if [ $# -eq 0 ]; then
+	manual()
 fi
+
+while getopts "richR" opt; do
+	case $opt in
+	r)
+		echo "mam flage r - szukanie rekursywnie"
+		;;
+	c)
+		echo "mam flage c - kolorowanie wyniku"
+		;;
+	h)	
+		echo "Instrukcja obsługi programu"
+		;;
+	i)
+		echo "Ignorowanie wielkości znaków"
+		;;
+	l)	
+		echo "Pobieranie wzorców z linii"
+		;;
+	R)	echo "Zamiana regexa"
+		;;
+	x)
+		echo "Usuwanie lini pasujących do wzorca"
+		;;
+	esac
+done
 
 
 
@@ -23,6 +39,7 @@ fi
 #znaczy
 #grep-- mamamamama mama*(nie pamietam skladni grepa ale rozumiecie, chodzi o wszystkie slowa zaczynajace sie na mama)
 #zwroci 1
+
 sprawdzane="$1"
 wzorzec="$2"
 
@@ -67,19 +84,5 @@ function fundamental () {
         i=$((i+1))
     done
 }
-#wzorzec pewnie trzeba będzie rozbić na czesci i kompletnie nie wiem o czym teraz pisze ale cos sie pojawilo w mojej glowie
-#"nana.mama" mozna na przyklad podzielic na 3 czesci, nana:.:mama i najpierw szukac czy w "sprawdzanym" jest 1 część, potem usuwać ją ze słowa sprawdzanego,
-# potem sprawdzać 2 część itd (dobrze zrobić tymczasowa zmienna na sprawdzana i wzorzec)
-#jezeli uda sie calkowicie calkowicie usunac oba wyrazy to wzorzec zostal spelniony (inaczej jest w przypadku znaku gwiazdki gdzie dla niego musi byc obsluga osobna)
-
-#o to co jest nizej dobrze byloby rozbic na tablice stringow, wtedy nie usuwa sie wyrazow tylko sprawdzasz czy uda ci sie dojsc do konca tablicy
-#a obsluga takiej "gwiazdki" na przyklad bylaby duzo latwiejsza
 
 fundamental $sprawdzane
-
-
-
-#echo "$sprawdzane"
-#echo "$wzorzec"
-
-#ogolnie to nie wiem czy to ma sens, jestem chory i to taki draft na szybko ktory mi sie w glowie zrobil
